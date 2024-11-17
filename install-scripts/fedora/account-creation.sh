0cd8303cc128ae246089c88ebd0ae536eb5084ce
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-11-17 16:23:58 +0800

    Update account-creation.sh

--- install-scripts/fedora/account-creation.sh
+++ install-scripts/fedora/account-creation.sh
@@ -2,13 +2,28 @@
 
 read -p "Please enter your username: " username
 
-# Add an extra space to the username
+# Remove any extra space from the username
 username="$username"
 
+# Add the user and assign to the wheel group
 useradd -m -G wheel $username
 
+# Set the password for the user
 passwd $username
 
-echo "account created"
+# Insert the username with sudo privileges at line 101, column 0 in /etc/sudoers file
+# Create a temporary file
+tmpfile=$(mktemp)
+
+# Insert the username with sudo privileges at the desired location
+awk -v username="$username" '{
+    if (NR == 101) {
+        print username " ALL=(ALL) ALL"
+    }
+    print $0
+}' /etc/sudoers > "$tmpfile" && mv "$tmpfile" /etc/sudoers
+
+echo "Account created and username with sudo privileges added to /etc/sudoers."
+
 
 exit 0
