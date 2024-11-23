8817e4a29af6849840a0812f75b9d66eb5153b4e
Welpyes <110968684+Welpyes@users.noreply.github.com>
2024-11-24 02:46:31 +0800

    Update main-install.sh

--- install-scripts/fedora/i3-method/main-install.sh
+++ install-scripts/fedora/i3-method/main-install.sh
@@ -23,7 +23,7 @@ SCRIPT_DIR="~/.autostarts"
 for script in "$SCRIPT_DIR"/*.sh; do
     if [ -x "$script" ]; then
         echo "Executing $script"
-        "$script"
+        "bash $script"
     else
         echo "$script is not executable. Skipping."
     fi
