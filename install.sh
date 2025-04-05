70238c580a5f41e35726db0cc006775e31595d8d
Welpyes <110968684+Welpyes@users.noreply.github.com>
2025-04-05 12:49:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ termux-x11 :0 >/dev/null &
 
 sleep 3
 
-am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+# am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 
 proot-distro login $distro_proot --shared-tmp -- /bin/bash -c 'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=\${TMPDIR} && su - $username -c "env DISPLAY=:0 $window_manager"'
