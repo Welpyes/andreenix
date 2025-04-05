8c84796cd87443d3f801b96a879395a2ce354f43
Welpyes <110968684+Welpyes@users.noreply.github.com>
2025-04-05 15:39:57 +0800

    Create fedora-package.sh

---
+++ fedora-package.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+read -p "Please enter your username: " username
+
+# Remove any extra space from the username
+username="$username"
+
+# Add the user and assign to the wheel group
+useradd -m -G wheel $username
+
+# Set the password for the user
+passwd $username
+
+# Insert the username with sudo privileges at line 101, column 0 in /etc/sudoers file
+# Create a temporary file
+tmpfile=$(mktemp)
+
+# Insert the username with sudo privileges at the desired location
+awk -v username="$username" '{
+    if (NR == 101) {
+        print username " ALL=(ALL) ALL"
+    }
+    print $0
+}' /etc/sudoers > "$tmpfile" && mv "$tmpfile" /etc/sudoers
+
+echo "Account created and username with sudo privileges added to /etc/sudoers."
+
+dnf update -y
+dnf upgrade -y 
+
+dnf install xfce4-session xfce4-panel xfce4-settings xfwm4 xfce4-taskmanager xfce4-screenshooter xfce4-dict xfce4-notifyd xfce4-clipman-plugin xfce4-weather-plugin xfce4-sensors-plugin feh ImageMagick rhythmbox firefox zsh cava kitty geany leafpad fastfetch xfdesktop viewnior pcmanfm  -y
+
+
+
+
+exit 0
