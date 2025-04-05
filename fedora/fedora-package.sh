e5115b85a70de85f10b04d48cdd7b40dadff4939
Welpyes <110968684+Welpyes@users.noreply.github.com>
2025-04-05 20:57:04 +0800

    Update fedora-package.sh

--- fedora/fedora-package.sh
+++ fedora/fedora-package.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+LGreen='\033[1;32m'
+NC='\033[0m'
+LY='\033[1;33m'
+
 read -p "Please enter your username: " username
 
 # Remove any extra space from the username
@@ -25,12 +29,29 @@ awk -v username="$username" '{
 
 echo "Account created and username with sudo privileges added to /etc/sudoers."
 
+# Package install prompt
+echo -e "${LGreen}Leaving it blank will install xfce(minimal) and additional packages ${NC}"
+
+read -p "List The packages you want to be installed: " packages
+
+packages="$packages"
+
+# package listing
+if [ "$packages" = "" ]; then
+    packages="xfce4-session xfce4-panel xfce4-settings xfwm4 xfce4-taskmanager xfce4-screenshooter xfce4-dict xfce4-notifyd xfce4-clipman-plugin xfce4-weather-plugin xfce4-sensors-plugin feh ImageMagick rhythmbox firefox xfce4-terminal leafpad fastfetch xfdesktop viewnior pcmanfm"
+    
+    echo "installing $packages"
+else 
+    echo "Initializing script"
+fi
+
 dnf update -y
 dnf upgrade -y 
+dnf install $packages -y
 
-dnf install xfce4-session xfce4-panel xfce4-settings xfwm4 xfce4-taskmanager xfce4-screenshooter xfce4-dict xfce4-notifyd xfce4-clipman-plugin xfce4-weather-plugin xfce4-sensors-plugin feh ImageMagick rhythmbox firefox zsh cava kitty geany leafpad fastfetch xfdesktop viewnior pcmanfm  -y
+rpm -e --nodeps xfce-polkit
 
-
+# dnf install xfce4-session xfce4-panel xfce4-settings xfwm4 xfce4-taskmanager xfce4-screenshooter xfce4-dict xfce4-notifyd xfce4-clipman-plugin xfce4-weather-plugin xfce4-sensors-plugin feh ImageMagick rhythmbox firefox zsh cava kitty geany leafpad fastfetch xfdesktop viewnior pcmanfm  -y
 
 
 exit 0
